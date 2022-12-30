340c2af84fde1aa9ba045cbe59e388527ad31cc5
anandece348 <andndece2608@gmail.com>
2022-12-30 20:34:09 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import '../styles/App.css';
-import {Welcome} from "./Welcome";
+import Welcome from "./Welcome";
  
-
 const App = () => {
   return (
     <Welcome name=""/>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,8 @@
 // write code for Welcome component here
  import React from "react";
- export const Welcome = () =>{
+ export const Welcome = (props) =>{
     return <div>
-        <h1>Hey !</h1>
+        <h1>Hey {props.name}!</h1>
         <h2>Welcome to Newton School.</h2>
     </div>
 }
